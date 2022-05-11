f2a19c51c4fcadf7b115f8a497df37791fb741d0
lyf5 <lyf5@yahoo.com>
2022-05-11 11:32:55 +0800

    liyufei.new@gmail.com

---
+++ src/components/TobeMint/TobeMint.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react'
+import { TobeMint } from './TobeMint'
+
+test(`Renders TobeMint`, () => {
+  render(<TobeMint />)
+})
---
+++ src/layout/Root.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+
+import { Marketplace, Mintplace, Profile, Connect } from './'
+//import { Header, PrivateRoute, TransactionProgress } from '../components'
+import { Header, TransactionProgress } from '../components'
+import { Container } from 'theme-ui'
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
+const Root = () => {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Connect>
+          <Container>
+            <Routes>
+              <Route path="/profile" element={<Profile/>} />
+              <Route path="/" element={<Marketplace/>} />
+              <Route path="/Mintplace" element={<Mintplace/>} />
+            </Routes>
+            <TransactionProgress />
+          </Container>
+        </Connect>
+      </Web3ReactProvider>
+    </Router>
+  )
+}
+
+export { Root }
